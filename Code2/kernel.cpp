fa54474d59fbc37735faa194dd96e2e650b9d9e3
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2018-10-14 16:11:08 +0530

    linker loader files updated

--- Code2/kernel.cpp
+++ Code2/kernel.cpp
@@ -1,5 +1,15 @@
+
+void printf(char* str)
+{
+	(unsigned short*)VideoMemory = (unsigned short*)0xb8000;
+
+	for(int i=0; str[i]!= '\0'; ++i)
+		VideoMemory[i] =str[i];
+}
+
 void kernelMain(void* multiboot_structure, unsigned int magicnumber)
 {
 	printf("Hello World!");
 	while(1);
+
 }
